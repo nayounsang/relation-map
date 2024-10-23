3ee7291142e8b6da375a280eb8e4abbfae374fb9
nayounsang <nayounsang722@gmail.com>
2024-10-23 17:03:10 +0900

    chore: install eslint plugin for jest

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,16 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
+import tseslint, { parser } from "typescript-eslint";
 
 /**
  * **recommended rules from `@eslint/js`**
  * - usage: `pluginJs.configs.recommended`
  * - [link](https://github.com/eslint/eslint/blob/main/packages/js/src/configs/eslint-recommended.js)
- * 
+ *
  * **recommended rules from `typescript-eslint`**
  * - usage: `...tseslint.configs.recommended`
  * - [link](https://github.com/typescript-eslint/typescript-eslint/blob/main/packages/eslint-plugin/src/configs/recommended.ts)
- * 
+ *
  * If you want to ignore some of these,
  * ```
  * rules: {
@@ -19,8 +19,15 @@ import tseslint from "typescript-eslint";
  * ```
  */
 export default [
-  {files: ["src/**/*.{js,mjs,cjs,ts}"]},
-  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
+  {
+    files: ["src/**/*.{js,mjs,cjs,ts}"],
+    languageOptions: {
+      globals: { ...globals.browser, ...globals.node },
+      parser,
+    },
+    rules: {
+      ...pluginJs.configs.recommended.rules,
+      ...tseslint.configs.recommended.rules,
+    },
+  },
 ];
