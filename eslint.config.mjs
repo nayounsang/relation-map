0e54155a3738abfa5a94e17cb8dedc52a6ae1a60
nayounsang <nayounsang722@gmail.com>
2024-10-23 16:37:12 +0900

    comment: wrtie js docs about recommended rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,7 +2,15 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 
-
+/**
+ * **recommended rules from `@eslint/js`**
+ * - usage: `pluginJs.configs.recommended`
+ * - [link](https://github.com/eslint/eslint/blob/main/packages/js/src/configs/eslint-recommended.js)
+ * 
+ * **recommended rules from `typescript-eslint`**
+ * - usage: `...tseslint.configs.recommended`
+ * - [link](https://github.com/typescript-eslint/typescript-eslint/blob/main/packages/eslint-plugin/src/configs/recommended.ts)
+ */
 export default [
   {files: ["src/**/*.{js,mjs,cjs,ts}"]},
   {languageOptions: { globals: {...globals.browser, ...globals.node} }},
