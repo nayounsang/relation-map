3ab5dc83e59d04fabf4b0295b3a2db6677a78f24
nayounsang <nayounsang722@gmail.com>
2024-10-27 00:05:54 +0900

    feat: export ug

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
+import { UndirectedGraph } from "./undirected-graph/undirected-graph";
 import { Tree } from "./tree/tree";
 import { OneToManyMap } from "./one-to-many-map/one-to-many-map";
 import { BiMap } from "./bi-map/bi-map";
 export { BiMap };
 export { OneToManyMap };
 export { Tree };
+export { UndirectedGraph };
